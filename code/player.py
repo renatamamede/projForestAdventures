0ed335294c3d2468967e3f2d37208e365bd7f7b1
Renata Mamede <renatamamede90@gmail.com>
2025-03-28 19:57:34 -0300

    Changing player attack, adding attack movement

--- code/player.py
+++ code/player.py
@@ -13,9 +13,8 @@ class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
         self.atk_delay = ENTITY_ATK_DELAY[self.name]
-
-#==================================================================================
-        self.images = [
+        # ==================================================================================
+        self.walk = [
             pygame.image.load(f'./asset/{name}.png'),
             pygame.image.load(f'./asset/{name}-2.png'),
             pygame.image.load(f'./asset/{name}-3.png'),
@@ -25,13 +24,24 @@ class Player(Entity):
             pygame.image.load(f'./asset/{name}-7.png'),
             pygame.image.load(f'./asset/{name}-8.png'),
         ]
-        self.image_index = 0
-        self.surf = self.images[self.image_index]
-#===================================================================================
+        self.walk_index = 0
+        self.surf = self.walk[self.walk_index]
+
+        self.atk_img = [
+            pygame.image.load(f'./asset/{name}Atk-1.png'),
+            pygame.image.load(f'./asset/{name}Atk-2.png'),
+            pygame.image.load(f'./asset/{name}Atk-3.png'),
+            pygame.image.load(f'./asset/{name}Atk-4.png'),
+            pygame.image.load(f'./asset/{name}Atk-5.png')
+        ]
+        self.atk_index = 0
+        self.surf = self.atk_img[self.atk_index]
+
+    # ===================================================================================
 
     def move(self):  # O ponto 0,0 no plano cartesiano do movimento está no canto superior esquerdo da tela.
-        pressed_key = pygame.key.get_pressed()
         moved = True
+        pressed_key = pygame.key.get_pressed()
         if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 150:  # Coloquei valores aqui e no K_DOWN para limitar o espaço em que o personagem vai se mover. Não quero que ande além do "caminho".
             self.rect.centery -= ENTITY_SPEED[self.name]  # O player é movimentado mexendo no RECT dele
         if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < 485:  # Use IF. Se for ELIF, o jogo vai entender que só pode se pressionar uma tecla de caa vez. Não é a ideia aqui.
@@ -41,8 +51,9 @@ class Player(Entity):
         if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:
              self.rect.centerx += ENTITY_SPEED[self.name]
         if moved:
-            self.image_index = (self.image_index + 1) % len(self.images)
-            self.surf = self.images[self.image_index]
+            self.walk_index = (self.walk_index + 1) % len(self.walk)
+            self.surf = self.walk[self.walk_index]
+
 
     def atk(self):
         self.atk_delay -=1
@@ -50,4 +61,8 @@ class Player(Entity):
             self.atk_delay = ENTITY_ATK_DELAY[self.name]
             pressed_key = pygame.key.get_pressed()
             if pressed_key[PLAYER_KEY_ATK[self.name]]:
+                attacking = True
+                if attacking:
+                    self.atk_index = (self.atk_index + 1) % len(self.atk_img)
+                    self.surf = self.atk_img[self.atk_index]
                 return PlayerAtk(name=f'{self.name}Atk', position=(self.rect.centerx, self.rect.centery))
